7b15ed83776681c6799561574888cb096e744bd5
Berkeban <ibberkeban@gmail.com>
2026-08-28 00:20:14 +0300

    Debugged and added Enable pin

--- src/ALU_Output_Reg.v
+++ src/ALU_Output_Reg.v
@@ -2,6 +2,7 @@ module ALU_OUTPUT_REG (
     input clk,
     input rst,
     input [31:0] ALU_OUTPUT_atALU,
+    input ALU_OUTPUT_REG_ENABLE,
 
     output reg [31:0] ALU_REG_OUT
 );
@@ -10,8 +11,9 @@ module ALU_OUTPUT_REG (
         if (rst == 1'b1) begin
             ALU_REG_OUT <= 32'b0;
         end
-        else
+        else if (ALU_OUTPUT_REG_ENABLE == 1'b1) begin
             ALU_REG_OUT <= ALU_OUTPUT_atALU;
+        end
     end
     
 endmodule
